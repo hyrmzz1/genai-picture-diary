86dcf3456b35b90de1d8e71316e43e4b9bcf866e
kingbob <kingbob@royglobal.net>
2024-08-01 22:50:58 +0900

    Feat: tailwind 컬러 설정

--- .devcontainer/frontend/tailwind.config.js
+++ .devcontainer/frontend/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+//const colors = require("tailwindcss/colors");
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      //Grayscale Colors
+      white: "#FFFFFF",
+      gray50: "#FAFAFA",
+      gray100: "#F5F5F5",
+      gray200: "#EFEFEF",
+      gray300: "#E2E2E2",
+      gray400: "#BFBFBF",
+      gray500: "#A0A0A0",
+      gray600: "#77777",
+      gray700: "#636363",
+      gray800: "#44444",
+      gray900: "#232527",
+      black: "#00000",
+      //State Colors
+      red: "#FF0101",
+      orange: "#FFB724",
+      blue: "#2768FF",
+    },
   },
   plugins: [],
-}
-
+};
