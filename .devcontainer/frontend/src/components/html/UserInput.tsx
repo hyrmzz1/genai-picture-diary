d5bb5d6dfd627e5a409640b1f7e28034519e4fd4
Hyerim Yang <hlhlstar@naver.com>
2024-08-03 23:05:52 +0900

    Feat: 인풋 컴포넌트 생성

---
+++ .devcontainer/frontend/src/components/html/UserInput.tsx
@@ -0,0 +1,38 @@
+interface UserInputProps {
+  label: string;
+  placeholder: string;
+  type: string;
+  value: string;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean; // 선택적 prop
+}
+
+const UserInput = ({
+  label,
+  placeholder,
+  type,
+  value,
+  name,
+  onChange,
+  required = true,
+}: UserInputProps) => {
+  return (
+    <>
+      <label className="block mb-[20px] text-[14px]">
+        {label}
+        <input
+          className="appearance-none bg-gray50 border border-gray400 focus:bg-white focus:border-blue focus:outline-none rounded-md w-full py-[20px] px-[16px] text-text_default mt-2"
+          type={type}
+          placeholder={placeholder}
+          value={value} // 전송할 입력값 지정
+          name={name}
+          onChange={onChange}
+          required={required}
+        />
+      </label>
+    </>
+  );
+};
+
+export default UserInput;
