9d665808e2a132282e4462108c92e842b7afa6f6
Hyerim Yang <hlhlstar@naver.com>
2024-08-03 23:04:17 +0900

    Feat: Gnb Logo - 본문 Logo 분리

---
+++ .devcontainer/frontend/src/components/html/NavLogo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const NavLogo = () => {
+  return (
+    <Link to="/">
+      <h1>Logo</h1>
+    </Link>
+  );
+};
+
+export default NavLogo;
---
+++ .devcontainer/frontend/src/components/html/PageLogo.tsx
@@ -0,0 +1,10 @@
+const PageLogo = () => {
+  return (
+    <div className="h-[162px] flex flex-col">
+      <div className="h-1/2 w-full"></div>
+      <div className="h-1/2 w-full text-center text-[40px]">Logo</div>
+    </div>
+  );
+};
+
+export default PageLogo;
