e46641b87e5a7d4e6313bbc50c6aae460beddb49
Hyerim Yang <hlhlstar@naver.com>
2024-08-04 11:52:53 +0900

    Refactor: 체크박스 svg 및 토글 적용

--- .devcontainer/frontend/src/components/html/CheckBox.tsx
+++ .devcontainer/frontend/src/components/html/CheckBox.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import RememberMeOnIcon from "../../assets/rememberMeOnIcon.svg";
+import RememberMeOffIcon from "../../assets/rememberMeOffIcon.svg";
+
 interface CheckBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   labelClassName?: string;
@@ -9,10 +13,26 @@ const CheckBox = ({
   labelClassName = "",
   inputClassName = "",
 }: CheckBoxProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <>
       <label className={`text-[14px] flex items-center ${labelClassName}`}>
-        <input type="checkbox" className={`${inputClassName}`} />
+        <input
+          type="checkbox"
+          className="hidden"
+          checked={isChecked}
+          onClick={handleToggle}
+        />
+        <img
+          src={isChecked ? RememberMeOnIcon : RememberMeOffIcon}
+          alt="Remember Me Check box"
+          className={`${inputClassName}`}
+        />
         {label}
       </label>
     </>
