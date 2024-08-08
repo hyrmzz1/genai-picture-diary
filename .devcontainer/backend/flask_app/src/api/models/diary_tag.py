1647648b202050af7c719251d4b6751a7c2799a8
kkumsogro <kkumsogro@gmail.com>
2024-08-08 15:17:22 +0900

    Feat: 일기(다이어리, 이미지, 태그, 댓글) 모델별 CRUD 함수 추가와 모델별 CRUD 기능 테스트

--- .devcontainer/backend/flask_app/src/api/models/diary_tag.py
+++ .devcontainer/backend/flask_app/src/api/models/diary_tag.py
@@ -3,13 +3,11 @@ from src.api.models.base import AdminBase, BaseModel, g_db
 
 class DiaryTag(BaseModel):
     __tablename__ = 'diary_tag'
-    tag_id = g_db.Column(g_db.Integer, primary_key=True)
-    entry_id = g_db.Column(g_db.Integer, g_db.ForeignKey('diary_entry.entry_id'), nullable=False)
+    entry_id = g_db.Column(g_db.Integer, g_db.ForeignKey('diary_entry.id'), nullable=False)
     tag_name = g_db.Column(g_db.String(50), nullable=False)
-    created_at = g_db.Column(g_db.DateTime, default=g_db.func.current_timestamp())
-    updated_at = g_db.Column(g_db.DateTime, default=g_db.func.current_timestamp(), onupdate=g_db.func.current_timestamp())
+    date_updated = g_db.Column(g_db.DateTime, default=g_db.func.current_timestamp(), onupdate=g_db.func.current_timestamp())
     
-    diary_entry = g_db.relationship('DiaryEntry', back_populates='diary_tag')  
+    entry = g_db.relationship('DiaryEntry', back_populates='tags')  
 
     def __init__(self, entry_id, tag_name):
         self.entry_id = entry_id
@@ -24,8 +22,27 @@ class DiaryTag(BaseModel):
 
     def __str__(self):
         return self.tag_name
+        
+    # create : base의 add_instance 함수 사용
+    # read : base의 get_instance 함수 사용
+    
+   # 태그 업데이트 메서드
+    def update_tag(self, data):
+        for key, value in data.items():
+            if key in self.__table__.columns:
+                setattr(self, key, value)
+        g_db.session.commit()
+
+    # 태그 삭제 메서드
+    def delete_tag(self, id):
+        tag = g_db.session.query(DiaryTag).get(id)
+        if not tag:
+            print('태그를 찾을 수 없음')
+            return
+        g_db.session.delete(tag)
+        g_db.session.commit()
 
 # ------------------------------------------ Admin ------------------------------------------   
 class DiaryTagAdmin(AdminBase):
     # 1. 표시 할 열 설정
-    column_list = ('tag_id', 'entry_id', 'tag_name', 'created_at', 'updated_at')
+    column_list = ('id', 'entry_id', 'tag_name', 'date_created', 'date_updated')
