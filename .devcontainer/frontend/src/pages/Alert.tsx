d523a6a02ef2fd88e59fdd07e61c901c890525cd
Hyerim Yang <hlhlstar@naver.com>
2024-09-14 21:50:35 +0900

    Feat: 알림 UI 개선 및 조건부 렌더링 추가
    - 알림이 없을 때와 있을 때의 조건부 렌더링 추가
    - AlertList 및 AlertListItem의 UI 및 스타일링 개선
    - 알림 리스트에 스크롤 적용
    - 전역 상태 관리 대신 mockAlerts 통해 개발 진행 (UI 확인하기 위함)
    - 단위 변환 (px to rem)

--- .devcontainer/frontend/src/pages/Alert.tsx
+++ .devcontainer/frontend/src/pages/Alert.tsx
@@ -2,16 +2,51 @@ import { useState, useEffect } from "react";
 import NoAlertIcon from "../assets/alertNoneIcon.svg?react";
 import SortButton from "../components/SortButton";
 import AlertList from "../components/AlertList";
-import useAlertStore from "../stores/alertStore";
+// import useAlertStore from "../stores/alertStore";
+
+const mockAlerts = [
+  {
+    id: 1,
+    title: "새로운 공지사항",
+    message: "새로운 공지사항이 있습니다.",
+    date: "2024-09-10T10:30:00Z",
+  },
+  {
+    id: 2,
+    title: "이벤트 알림",
+    message:
+      "이벤트 참여하세요!🎉 이벤트 참여하세요!🎊 이벤트 참여하세요!🎉 이벤트 참여하세요!🎊 이벤트 참여하세요!🎉 이벤트 참여하세요!🎊 이벤트 참여하세요!🎉 이벤트 참여하세요!🎊 ",
+    date: "2024-09-14T12:12:00Z",
+  },
+  {
+    id: 3,
+    title: "업데이트 알림",
+    message: "A 기능 지원이 중단되었습니다.",
+    date: "2024-09-06T09:00:00Z",
+  },
+  {
+    id: 4,
+    title: "업데이트 알림",
+    message: "업데이트 내용을 확인하세요.",
+    date: "2024-09-09T15:45:00Z",
+  },
+  {
+    id: 5,
+    title: "새로운 공지사항",
+    message: "닉네임 설정 기능이 생겼어요!",
+    date: "2024-09-13T19:00:00Z",
+  },
+];
 
 const Alert = (): JSX.Element => {
-  const alertList = useAlertStore((state) => state.alerts);
-  const fetchAlerts = useAlertStore((state) => state.fetchAlerts);
+  // const alertList = useAlertStore((state) => state.alerts);
+  // const fetchAlerts = useAlertStore((state) => state.fetchAlerts);
+  const [alertList, setAlertList] = useState(mockAlerts);
   const [sortOrder, setSortOrder] = useState<"latest" | "oldest">("latest");
 
-  useEffect(() => {
-    fetchAlerts();
-  }, [fetchAlerts]);
+  // useEffect(() => {
+  //   fetchAlerts();
+  // }, [fetchAlerts]);
 
   const sortedAlerts = [...alertList].sort((a, b) => {
     return sortOrder === "latest"
@@ -21,9 +56,9 @@ const Alert = (): JSX.Element => {
 
   return (
     <div className="flex justify-center">
-      <div className="w-[900px] my-10 flex flex-col gap-y-4">
+      <div className="w-[56.25rem] my-10 flex flex-col gap-y-4">
         <p className="text-lg font-extrabold">내 소식</p>
-        <div className="min-h-[535px] p-6 flex flex-col bg-white rounded-xl drop-shadow-[0_5px_5px_rgba(0,0,0,0.1)]">
+        <div className="min-h-[33.4375rem] p-6 flex flex-col bg-white rounded-xl drop-shadow-[0_.3125rem_.3125rem_rgba(0,0,0,0.1)]">
           {/* 정렬 버튼 */}
           <div className="flex justify-end gap-4">
             <SortButton
@@ -39,17 +74,23 @@ const Alert = (): JSX.Element => {
               onClick={() => setSortOrder("oldest")}
             />
           </div>
-          {/* TODO) 소식 여부에 따른 조건부 렌더링 구현 필요 */}
-          <div className="w-full h-full">
+
+          {/* 알림 item */}
+          <div
+            className={`w-full h-[28.125rem] flex flex-col gap-y-4 ${sortedAlerts.length ? "" : "justify-center items-center"}`}
+          >
             {/* 소식 없을 때 */}
-            <div className="w-full h-full flex flex-col justify-center items-center gap-y-4">
-              <NoAlertIcon />
-              <p className="text-sm text-text_info">새로운 소식이 없습니다.</p>
-            </div>
+            {!sortedAlerts.length && (
+              <>
+                <NoAlertIcon />
+                <p className="text-sm text-text_info">
+                  최근 30일동안 받은 알림이 없어요
+                </p>
+              </>
+            )}
+
             {/* 소식 있을 때 */}
-            <div className="w-full h-full flex flex-col">
-              <AlertList alerts={sortedAlerts} />
-            </div>
+            {sortedAlerts.length > 0 && <AlertList alerts={sortedAlerts} />}
           </div>
         </div>
       </div>
